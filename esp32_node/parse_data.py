c912e106d20efb1c45d79e974e9cdc9c1f7acb69
Bianca Espindola <32000570+BiancaEspindola@users.noreply.github.com>
2021-11-08 21:29:37 -0300

    using a bigger ECG data from physionet to increase network and internal storage usage

--- esp32_node/parse_data.py
+++ esp32_node/parse_data.py
@@ -1,12 +1,12 @@
 import wfdb
 import numpy as np
 np.set_printoptions(threshold=np.inf)
-signals, fields = wfdb.rdsamp(r"ecg_data/ecg_data",channels=[1])
+signals, fields = wfdb.rdsamp(r"ecg_data/ecg_data",channels=[0],sampto=175000)
 wfdb.plot_items(signal=signals,fs=fields['fs'], title='ECG')
 # Display it with the additional information
 # display((signals, fields))
 print(signals.tobytes(order='C'))
 print(fields)
-# with open("dump.txt", 'wb') as f:
-#     f.write(signals.tobytes(order='C'))
+with open("dump.txt", 'wb') as f:
+    f.write(signals.tobytes(order='C'))
     
