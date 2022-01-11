167a4a53f42b9b5b341972267d3c787303827e9c
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2022-01-10 22:36:40 -0300

    ajustando o arquivo server para suportar conexoes pelo express e pelo ws

--- webserver/server/src/server.ts
+++ webserver/server/src/server.ts
@@ -1,8 +1,18 @@
 import express, { json, request, response } from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+
 import "./database";
 import { routes } from "./routes";
 
 const app = express(); 
+
+const http = createServer(app); //criando protocolo http
+const io = new Server(http); //criando protocolo websocket
+
+io.on("connection", (socket: Socket)=> {
+    console.log("conectado via websocket", socket.id);
+})
 
 // app.get("/", (request, response) => {
 //     return response.json({
@@ -20,6 +30,6 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, ()  => console.log("O servidor está rodando na porta 3333"));
+http.listen(3333, ()  => console.log("O servidor está rodando na porta 3333"));
 
 
