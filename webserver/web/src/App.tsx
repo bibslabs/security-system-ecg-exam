df07484ea95e69cd17026f1d261c51bb75a235df
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2022-01-14 21:59:01 -0300

    definição de variaveis globais css

--- webserver/web/src/App.tsx
+++ webserver/web/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './assets/styles/global.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Word</h1>
+      <h1>Exame ECG</h1>
+      <button type='button'>Iniciar conexão</button>
     </div>
   );
 }
