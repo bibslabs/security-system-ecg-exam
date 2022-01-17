a9d6e1f8c9690bee473b711bd1f4f5c08e53ffe6
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2022-01-17 16:07:16 -0300

    landing mpage mobile first

--- webserver/web/src/App.tsx
+++ webserver/web/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import Landing from './pages/Landing';
 import './assets/styles/global.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Exame ECG</h1>
-      <button type='button'>Iniciar conexão</button>
-    </div>
+    // <div className="App">
+    //   <h1>Exame ECG</h1>
+    //   <button type='button'>Iniciar conexão</button>
+    // </div>
+    <Landing />
   );
 }
 
---
+++ webserver/web/src/pages/Landing/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import logoImg from '../../assets/images/logo-header.png'
+
+import loginIcon from "../../assets/images/icons/fi-rr-sign-in-alt.svg"
+import adminIcon from "../../assets/images/icons/fi-rr-incognito.svg"
+
+import './styles.css'
+
+function Landing() {
+    return (
+       <div id="page-landing">
+           <div id="page-landing-content" className="container">
+               <div className="logo-container">
+                   <img src={logoImg} alt="Dados Seguros" />
+                   <h2>Dados seguros, saúde garantida</h2>
+               </div>
+               
+               <div className="buttons-container">
+                    <a href="" className="login">
+                        <img src={loginIcon} alt="Login" />
+                        Login
+                    </a> 
+
+                    <a href="" className="admin">
+                        <img src={adminIcon} alt="Admin Login" />
+                        Admin
+                    </a>
+               </div>
+           </div>
+       </div> 
+    )
+}
+
+export default Landing;
