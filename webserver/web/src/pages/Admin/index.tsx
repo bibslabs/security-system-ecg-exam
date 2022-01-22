591aa3d366181f97fd0576bd834fda0c412ff77b
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2022-01-21 21:36:53 -0300

    criação botão para rora get teste

--- webserver/web/src/pages/Admin/index.tsx
+++ webserver/web/src/pages/Admin/index.tsx
@@ -1,9 +1,58 @@
-import React from "react";
+import React, {useState, FormEvent} from "react";
+
+import api from "../../services/api";
+
+import './styles.css';
 
 function Admin() {
+    // const [username, setUsername] = useState('');
+    const handleSubmit = async () => {
+
+        try {
+            const response =  await api.get('/admin',{})
+
+            console.log(response);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    // onClick={handleSubmit}
+
+
     return (
-        <h1>Rota do admin</h1>
+        <div id="page-admin">
+            <h1>Rota do admin</h1>
+            <button id="button-connection" onClick={handleSubmit} type="button">iniciar conexão</button> 
+        </div>
     )
 }
 
 export default Admin;
+
+// const Admin: React.FC = () => {
+//     const navigate = useNavigate();
+    
+
+//     const handleSubmit = async (event: FormEvent) => {
+//         event.preventDefault();
+
+//         try {
+//             await api.post('/admin', {
+//                 username
+//             })
+
+//             push('/admin');
+//         } catch (error) {
+//             console.error(error);
+//         }
+//     }
+
+
+//     return (
+//         <div id="page-admin">
+//             <h1>Rota do admin</h1>
+//             <button type="button" onClick={handleSubmit}>iniciar conexão</button>
+//         </div>
+
+        
+//     )
--- webserver/web/src/pages/Landing/index.tsx
+++ webserver/web/src/pages/Landing/index.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from 'react-router-dom'
 
 
-import logoImg from '../../assets/images/logo-header.png'
+import logoImg from '../../assets/images/logo-header.png';
 
-import loginIcon from "../../assets/images/icons/fi-rr-stethoscope.svg"
-import adminIcon from "../../assets/images/icons/fi-rr-following.svg"
+import loginIcon from "../../assets/images/icons/fi-rr-stethoscope.svg";
+import adminIcon from "../../assets/images/icons/fi-rr-following.svg";
 
-import './styles.css'
+// import api from '../../services/api';
+
+import './styles.css';
 
 function Landing() {
+    // const [connection, setConnection] = useState(0);
+
     return (
        <div id="page-landing">
            <div id="page-landing-content" className="container">
